396c156b19dcd03d288dc67da7953de42a638964
Josh VanderLinden <codekoala@gmail.com>
2016-03-14 14:10:14 -0600

    Switching to manual uploads of the base image
    For some reason, the automated builds have stopped appreciating my
    compressed tarball. "images" produced using it complain about /bin/bash
    not being accessible.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 DATE=$(date +"%Y.%m.%d")
 
 # remove any existing root filesystem from the repo history
-git filter-branch -f --tree-filter 'rm -f arch-rootfs*' --prune-empty master
+# git filter-branch -f --tree-filter 'rm -f arch-rootfs*' --prune-empty master
 
 # build an updated root filesystem
 sudo ./mkimage-arch.sh ${DATE}
@@ -15,4 +15,4 @@ sed "s/TAG/${DATE}/" Dockerfile.tpl > Dockerfile
 
 # commit the changes
 git add Dockerfile && git commit -m "Update Dockerfile (${DATE})"
-git add arch-rootfs-${DATE}.tar.xz && git commit -m "Update rootfs (${DATE})"
+# git add arch-rootfs-${DATE}.tar.xz && git commit -m "Update rootfs (${DATE})"
